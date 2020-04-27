e77dbf8efc7da0238752589b76d90a0851e14d4f
Raydel Miranda <raydel@everymundo.com>
2020-04-27 15:17:18 -0400

    Adding functionality for parsing Kinesis records retrieved using the aws-sdk library.

--- test/lib/parse-kinesis-record.test.js
+++ test/lib/parse-kinesis-record.test.js
@@ -82,4 +82,65 @@ describe('lib/parse-kinesis-record', () => {
       })
     })
   })
+
+  describe("#flatJsonSDKRecords", () => {
+
+    context('When each record contains a JSON array of objects', () => {
+      it('should return a flat array', () => {
+        const Records = [
+            { Data: 'H4sIAAAAAAAAA4uuVkpUsjKs1QHTRlDauDYWAK14sVAZAAAA', SequenceNumber: "00000", PartitionKey: "0000"},
+            { Data: 'W3siYSI6NH0seyJhIjo1fSx7ImEiOjZ9XQ==', SequenceNumber: "00000", PartitionKey: "0000"}
+        ]
+        const response = Array.from(lib.flatJsonSDKRecords(Records))
+        expect(response).to.have.property('length', 6)
+        expect(response).to.deep.equal([{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }, { a: 5 }, { a: 6 }])
+      })
+    })
+
+    context('When each record contains a single JSON objects', () => {
+      it('should return a flat array', () => {
+
+        const Records = [
+          { Data: 'H4sIAAAAAAAAA6tWSlSyMtRRSlKyMtJRSlayMq4FAKtb1C4TAAAA', SequenceNumber: "000", PartitionKey: "000" },
+          { Data: 'eyJhIjo0LCJiIjo1LCJjIjo2fQ==', SequenceNumber: "000", PartitionKey: "000" }
+        ]
+
+        const response = Array.from(lib.flatJsonSDKRecords(Records))
+
+        expect(response).to.have.property('length', 2)
+        expect(response).to.deep.equal([{ a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 }])
+      })
+    })
+  })
+
+  describe("#flatJsonSDKSingleRecord", () => {
+    context('When the record contains a single JSON objects', () => {
+      it('should return a flat array', () => {
+
+        const Record = {
+          Data: 'H4sIAAAAAAAAA6tWSlSyMtRRSlKyMtJRSlayMq4FAKtb1C4TAAAA',
+          SequenceNumber: "000",
+          PartitionKey: "000"
+        }
+        const response = Array.from(lib.flatJsonSDKSingleRecord(Record))
+
+        expect(response).to.have.property('length', 1)
+        expect(response).to.deep.equal([{ a: 1, b: 2, c: 3 }])
+      })
+    })
+    context('When the record contains a JSON array of objects', () => {
+      it('should return a flat array', () => {
+
+        const Record = {
+          Data: 'H4sIAAAAAAAAA4uuVkpUsjKs1QHTRlDauDYWAK14sVAZAAAA',
+          SequenceNumber: "000",
+          PartitionKey: "000"
+        }
+        const response = Array.from(lib.flatJsonSDKSingleRecord(Record))
+
+        expect(response).to.have.property('length', 3)
+        expect(response).to.deep.equal([{ a: 1 }, { a: 2 }, { a: 3 }])
+      })
+    })
+  })
 })
